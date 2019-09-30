f88c0cfb5ed759b9ae84f3ff274bf2a0fafda808
Glitch (hello-express) <none>
2019-09-30 21:10:32 +0000

    ✈️🐒 Checkpoint ./server.js:7275976/627

--- server.js
+++ server.js
@@ -9,7 +9,18 @@ app.get("/",(req,res)=>{
   res.send("API Project: Timestamp Microservice")
 })
 
-app.get("api/timestamp/:date_string?")
+app.get("/api/timestamp/:date_string?",(req,res)=>{
+
+  const date_string = req.params.date_string || "";
+
+  const date = new Date(date_string);
+
+  
+  let rep = err!=null ? {error:"Invalid Date"}:{unix:date.getTime(),utc:date.toUTCString()};
+
+  
+  res.send(rep);
+});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
