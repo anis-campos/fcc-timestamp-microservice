08acb5a12c134ea8eb95300df5e78cb4183602d0
Glitch (hello-express) <none>
2019-09-30 21:33:32 +0000

    🎥📅 Checkpoint ./server.js:7275976/360 ./package.json:7275976/1033

--- server.js
+++ server.js
@@ -4,20 +4,22 @@
 // init project
 const express = require('express');
 const app = express();
+const showdown = require("showdown");
 
 app.get("/",(req,res)=>{
-  res.send("API Project: Timestamp Microservice")
+  res.send(showdown)
 })
 
 app.get("/api/timestamp/:date_string?",(req,res)=>{
   
+  //parsing date
   const date = new Date(req.params.date_string);
-
+  
+  //getting utc anf unix/NaN values
   const [unix,utc] = [date.getTime(),date.toUTCString()];
   
-  const err = isNaN(unix);
-  
-  let rep = err ? {error:utc}:{unix:unix,utc:utc};
+  //if error (NaN) return utc as error (Invalid Date) otherwise return unix and utc
+  const rep = isNaN(unix) ? {error:utc} : {unix:unix,utc:utc};
 
   res.send(rep);
 });
